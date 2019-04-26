9da92ad688ebcf0186628d659221b58d2ed388fc
mehmetaligozbulut <mehmetgozbulut@gmail.com>
2019-04-26 13:46:06 +0300

    Atoms alert and button component test review

--- src/app/shared/components/atoms-alert/atoms-alert.component.spec.ts
+++ src/app/shared/components/atoms-alert/atoms-alert.component.spec.ts
@@ -2,38 +2,41 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { AtomsAlertComponent } from './atoms-alert.component';
 
-export interface UValidationErrorComponent {
+export interface IAtomsAlertComponent {
   component: AtomsAlertComponent;
   fixture: ComponentFixture<AtomsAlertComponent>;
 }
 
-describe('AtomsAlertComponent', function(this: UValidationErrorComponent) {
-  describe('as a unit', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [AtomsAlertComponent],
-      }).compileComponents();
+describe('AtomsAlertComponent', function(this: IAtomsAlertComponent) {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AtomsAlertComponent],
+    }).compileComponents();
 
-      this.fixture = TestBed.createComponent(AtomsAlertComponent);
-      this.component = this.fixture.componentInstance;
+    this.fixture = TestBed.createComponent(AtomsAlertComponent);
+    this.component = this.fixture.componentInstance;
+    this.fixture.detectChanges();
+  });
 
-      this.component.alertClass = 'primary';
-      this.component.alertText = 'Test';
-      this.fixture.detectChanges();
-    });
+  it('should be created', () => {
+    expect(this.component).not.toBeUndefined();
+  });
 
-    it('should be atoms-alert created', () => {
-      expect(this.component).not.toBeUndefined();
-    });
+  it('should have default alertText', () => {
+    expect(this.component.alertText).toBe('Alert example text');
+  });
 
-    it('should have alert alert-primary as class', () => {
-      const el: HTMLElement = this.fixture.debugElement.query(By.css('div')).nativeElement;
-      expect(el.className).toBe('alert alert-primary');
-    });
+  it('should have default alertClass', () => {
+    expect(this.component.alertClass).toBe('primary');
+  });
 
-    it('should have div elements alertText', () => {
-      const textContent = (this.fixture.debugElement.query(By.css('div')).nativeElement as HTMLElement).textContent;
-      expect(textContent.trim()).toEqual(this.component.alertText);
-    });
+  it('should have alert alert-primary as class', () => {
+    const el: HTMLElement = this.fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(el.className).toBe('alert alert-primary');
+  });
+
+  it('should have div elements alertText', () => {
+    const { textContent } = this.fixture.debugElement.query(By.css('div')).nativeElement as HTMLElement;
+    expect(textContent.trim()).toEqual(this.component.alertText);
   });
 });
